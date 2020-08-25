0baaa4c66113c3f1de45b8f2b8e13cb0b1d0881c
yysg <yysg@users.noreply.github.com>
2020-08-25 14:05:38 +0800

    Update setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,6 +3,16 @@ const proxy = require('http-proxy-middleware');
 const mainPort = process.env.PORT || '3000';
 const port = process.env.API_PORT || parseInt(mainPort, 10) + 1;
 module.exports = function(app) {
-  app.use(proxy('/api', { target: `http://ec2-52-53-200-7.us-west-1.compute.amazonaws.com:3001/` }));
-  app.use(proxy('/uploads', { target: `http://localhost:${port}/` }));
+  app.use(
+    proxy('/api', {
+      target: `http://ec2-52-53-200-7.us-west-1.compute.amazonaws.com:3001/`,
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    proxy('/uploads', {
+      target: `http://ec2-52-53-200-7.us-west-1.compute.amazonaws.com:3001/`,
+      changeOrigin: true,
+    })
+  );
 };
