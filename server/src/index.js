d565673c0cfeb171f1e5b87c6c53b7f1cd103155
yysg <707912214@qq.com>
2020-08-25 20:39:29 +0800

    add ip api

--- server/src/index.js
+++ server/src/index.js
@@ -36,6 +36,32 @@ app.post('/api/mlmodels', checkLoginMiddleware, (req, res) => {
     id,
   });
 });
+function getIPAddress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (
+        alias.family === 'IPv4' &&
+        alias.address !== '127.0.0.1' &&
+        !alias.internal
+      ) {
+        return alias.address;
+      }
+    }
+  }
+}
+
+app.get('/api/ip', (req, res) => {
+  const LOCAL_IP = getIPAddress();
+  console.log(LOCAL_IP);
+
+  res.json({
+    success: true,
+    ip: LOCAL_IP,
+  });
+});
 
 app.post('/api/mlmodels/:id', (req, res) => {
   const { id } = req.params;
